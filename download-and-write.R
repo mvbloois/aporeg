b4a256ad067e85391971965c09546fd274ec9379
Martijn van Bloois <mvbloois@gmail.com>
2024-05-22 20:49:23 +0200

    Update download-and-write.R

--- download-and-write.R
+++ download-and-write.R
@@ -1,18 +1,18 @@
-library(rvest)
-library(stringr)
 
 url <- "https://www.igj.nl/zorgsectoren/eerstelijnszorg/publicaties/publicaties/2017/08/17/register-van-gevestigde-apothekers"
 
 # read the html
-html <- read_html(url)
+html <- rvest::read_html(url)
 
 # extract the date after publicatiedatum
-date <- html |> 
-  html_nodes("p:contains('Publicatie')") |> 
-  html_text() |>
-  head(1) |> 
-  str_extract("\\d{2}-\\d{2}-\\d{4}") |> 
-  as.Date(format = "%d-%m-%Y")
+date_text <- html |> 
+  rvest::html_nodes("p:contains('Publicatie')") |> 
+  rvest::html_text() |>
+  tail(1) 
+
+# Use base R functions to extract the date
+date <- regmatches(date_text, regexpr("\\d{2}-\\d{2}-\\d{4}", date_text))
+date <- as.Date(date, format = "%d-%m-%Y")
   
 if (Sys.Date() == date) {
   url <- "https://www.igj.nl/binaries/igj/documenten/publicaties/2017/08/17/register-van-gevestigde-apothekers/Apothekersregister.csv"
